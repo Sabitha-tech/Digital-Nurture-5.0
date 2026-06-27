f26320a57518749e75e90e1f31c04d1e5f82d302
Sabitha P <sabithaperumal508@gmail.com>
2026-06-27 15:14:56 +0530

    Add JUnit setup exercise

---
+++ junit-setup-exercise/src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testGetGreeting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+        assertEquals("Hello World!", classUnderTest.getGreeting());
+    }
+}
